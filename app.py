9e6231ac33d26b94fbca87af2ff661cfe26d3c72
Kauan <jailma.kmp@gmail.com>
2022-03-04 22:38:31 -0300

    create done

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
-from flask import Flask, render_template
-from dataOperations import CatchData
+from flask import Flask, redirect, render_template, request, url_for
+from dataOperations import CatchData, CreateData
 
 # Flask
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", products = CatchData("crud_produtos.db", "products"), categorys = CatchData("crud_produtos.db", "category"))
+    return render_template("index.html", products = CatchData("products"), categorys = CatchData("category"))
+
+@app.route("/create_products", methods=['POST','GET'])
+def create_products():
+    if request.method == 'POST':
+        name = request.form['NAME'] 
+        price = request.form['PRICE']
+        description = request.form['DESCRIPTION']
+        category_id = request.form['CATEGORY_ID']
+        product_infos = [(name, price, description, category_id)]
+        CreateData("products", product_infos)
+        return redirect(url_for("index")) 
+        #Depois de realizar as operações na db fazer o redirect para o index
+
+    else:
+        return render_template("create_products.html")
+
+@app.route("/create_categorys", methods=['POST','GET'])
+def create_categorys():
+    if request.method == 'POST':
+        name = request.form['NAME'] 
+        description = request.form['DESCRIPTION']
+        category_infos = [(name, description)]
+        CreateData("category", category_infos)
+        return redirect(url_for("index")) 
+        #Depois de realizar as operações na db fazer o redirect para o index
+        
+    else:
+        return render_template("create_categorys.html")
--- dataOperations.py
+++ dataOperations.py
@@ -1,7 +1,27 @@
 import sqlite3
 
-def CatchData (database, table):
-    con=sqlite3.connect(database)
+def CatchData (table):
+    con=sqlite3.connect("crud_produtos.db")
     cur = con.cursor()
     cur.execute("SELECT * FROM {t};".format(t = table))
     return cur.fetchall()
+
+def CreateData (table, data):
+    con=sqlite3.connect("crud_produtos.db")
+    cur = con.cursor()
+    
+    if (table == "category"):
+        cur.execute("SELECT ID FROM category")
+        ids = cur.fetchall()
+        cur.executemany('''
+INSERT INTO {t} (ID, NAME, DESCRIPTION) VALUES ({id}, ?, ?)
+'''.format(t = table, id = ids[-1][-1] + 1), data) # ids[-1][-1] + 1 vai colocar o id da categoria como o ID anterior + 1
+
+    else:
+        cur.execute("SELECT ID FROM products")
+        ids = cur.fetchall()
+        cur.executemany('''
+INSERT INTO {t} (ID, NAME, PRICE, DESCRIPTION, CATEGORY_ID) VALUES ({id}, ?, ?, ?, ?)
+'''.format(t = table, id = ids[-1][-1] + 1), data) # Mesma logica
+
+    con.commit()
